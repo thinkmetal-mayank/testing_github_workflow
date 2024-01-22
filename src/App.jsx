a2cc060538e23b1e0f2c33e610f7c00f1d1b92e9
thinkmetal-mayank <mayank@thinkmetal.in>
2024-01-22 14:56:23 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ function App() {
   return (
     <>
       <h1>Vite + React</h1>
-      <div className="card">
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-      </div>
+      </div> */}
     </>
   )
 }
